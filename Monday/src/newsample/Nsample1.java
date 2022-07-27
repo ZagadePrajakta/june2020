5c47a4fe44a3710d591305dc348d1ccd51ba839c
praja <praja@Ajay>
2022-07-27 21:04:26 +0530

    this is 2 commit in monday

--- Monday/src/newsample/Nsample1.java
+++ Monday/src/newsample/Nsample1.java
@@ -5,5 +5,9 @@ public class Nsample1
       public static void main(String[] args)
    {
 	System.out.println("good morning");
+	int a=10;
+	int b=20;
+	int sum=a+b;
+	System.out.println(sum);
    }
 }
